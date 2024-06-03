f10942915708a2acbf24bd0dd5c4fe1a420cde0a
Lakshidaa <lakshidaa.saigiridharan@astrazeneca.com>
2024-06-03 15:01:23 +0200

    Remove support to download public .hdf5 model files

--- aizynthfinder/tools/download_public_data.py
+++ aizynthfinder/tools/download_public_data.py
@@ -8,10 +8,6 @@ import requests
 import tqdm
 
 FILES_TO_DOWNLOAD = {
-    "policy_model": {
-        "filename": "uspto_model.hdf5",
-        "url": "https://zenodo.org/record/7341155/files/uspto_keras_model.hdf5",
-    },
     "policy_model_onnx": {
         "filename": "uspto_model.onnx",
         "url": "https://zenodo.org/record/7797465/files/uspto_model.onnx",
@@ -19,10 +15,6 @@ FILES_TO_DOWNLOAD = {
     "template_file": {
         "filename": "uspto_templates.csv.gz",
         "url": "https://zenodo.org/record/7341155/files/uspto_unique_templates.csv.gz",
-    },
-    "ringbreaker_model": {
-        "filename": "uspto_ringbreaker_model.hdf5",
-        "url": "https://zenodo.org/record/7341155/files/uspto_ringbreaker_keras_model.hdf5",
     },
     "ringbreaker_model_onnx": {
         "filename": "uspto_ringbreaker_model.onnx",
@@ -36,10 +28,6 @@ FILES_TO_DOWNLOAD = {
         "filename": "zinc_stock.hdf5",
         "url": "https://ndownloader.figshare.com/files/23086469",
     },
-    "filter_policy": {
-        "filename": "uspto_filter_model.hdf5",
-        "url": "https://ndownloader.figshare.com/files/25584743",
-    },
     "filter_policy_onnx": {
         "filename": "uspto_filter_model.onnx",
         "url": "https://zenodo.org/record/7797465/files/uspto_filter_model.onnx",
@@ -52,7 +40,7 @@ YAML_TEMPLATE = """expansion:
     - {}
   ringbreaker:
     - {}
-    - {}     
+    - {}
 filter:
   uspto: {}
 stock:
